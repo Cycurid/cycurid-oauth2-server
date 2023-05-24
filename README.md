87c3f35ecf45d9acd440ad90151f5d1483467f87
Jordan <developer@cycurid.com>
2023-05-24 14:21:56 -0700

    updated the error handling and readme

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# Imme-Widget-Server
+# cycurid-oauth2-server
+
+The cycurid-oauth2-server npm package is a part of a comprehensive OAuth 2.0 solution designed to seamlessly integrate with the cycurid-oauth2-client package. This package primarily handles the process of exchanging an authorization code, obtained from the CycurID barcode scanning process, for an OAuth access token.
+
+This access token encapsulates the permissions as defined by the scope and allows the retrieval of user data that is both authorized by the user and compliant with server policies. By adhering to the OAuth's authorization code grant type, this process ensures a high level of security in authenticating and authorizing users while preserving the integrity and privacy of their credentials.
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to install cycurid-oauth2-server.
+
+```bash
+npm install cycurid-oauth2-server
+```
+
+## Usage
+
+### Import
+
+import Node:
+
+```javascript
+const { getToken, getUserInfo } = require('cycurid-oauth2-server');
+```
+
+### Supported methods
+
+#### getToken
+
+The getToken function retrieves an access token from an OAuth 2.0 server using the authorization code obtained from the CycurID barcode.
+
+### Example
+
+```javascript
+    const tokenResponse = await getToken(code: string, clientID: string, clientSecret: string)
+```
+
+On successful exchange, the function provides an access token that can be used to fetch user data as defined in the scope. If an error occurs during the process, an ErrorResponse is returned, providing details on the error encountered.
+
+#### getUserData
+
+The getUserInfo function in the cycurid-oauth2-server package is used to retrieve user information from an OAuth 2.0 server using the access token obtained through the getToken function.
+
+### Example
+
+```javascript
+   const userInfo = await getUserInfo(tokenResponse:string);
+```
+
+If the call is successful, userInfo will contain the user information as per the defined scope. If an error occurs during the process, userInfo will contain an error message detailing the problem.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+MIT License
+
+Copyright (c) 2022 CycurID
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+```
+
+```
--- src/utils/getToken.ts
+++ src/utils/getToken.ts
@@ -1,4 +1,4 @@
-import fetch, { Headers } from 'node-fetch';
+import fetch, { Headers, RequestInit } from 'node-fetch';
 import FormData from 'form-data';
 import { OAUTH_SERVER } from './constants';
 
@@ -8,54 +8,72 @@ interface TokenResponse {
 }
 
 interface ErrorResponse {
-  status?: number; // Add the optional 'status' property
-  statusText?: string; // Add the optional 'statusText' property
+  status: number;
+  statusText: string;
 }
 
-export async function getToken(
-  code: string,
-  clientID: string,
-  clientSecret: string,
-): Promise<string | undefined | ErrorResponse> {
+interface FetchOptions extends RequestInit {
+  headers: Headers;
+  body: FormData;
+}
+
+function createHeaders(clientID: string, clientSecret: string): Headers {
+  const data = `${clientID}:${clientSecret}`;
+  const base64data = Buffer.from(data, 'utf-8').toString('base64');
+  const headers = new Headers();
+  headers.append('Authorization', `Basic ${base64data}`);
+  return headers;
+}
+
+function createBody(code: string): FormData {
+  const formdata = new FormData();
+  formdata.append('grant_type', 'authorization_code');
+  formdata.append('scope', 'username');
+  formdata.append('code', code);
+  return formdata;
+}
+
+export async function getToken(code: string, clientID: string, clientSecret: string): Promise<string | ErrorResponse> {
+  if (!code || !clientID || !clientSecret) {
+    throw new Error('All arguments must be provided and be non-empty strings');
+  }
+
   try {
-    const data = `${clientID}:${clientSecret}`;
-    let token: string;
+    const headers = createHeaders(clientID, clientSecret);
+    const body = createBody(code);
 
-    const buff = Buffer.from(data);
-    const base64data = buff.toString('base64');
-    const myHeaders = new Headers();
-    myHeaders.append('Authorization', `Basic ${base64data}`);
-
-    const formdata = new FormData();
-    formdata.append('grant_type', 'authorization_code');
-    formdata.append('scope', 'username');
-    formdata.append('code', code);
-
-    const requestOptions = {
+    const requestOptions: FetchOptions = {
       method: 'POST',
-      headers: myHeaders,
-      body: formdata,
+      headers: headers,
+      body: body,
     };
 
     const response = await fetch(`${OAUTH_SERVER}/oauth/token`, requestOptions);
-
     const responseData: TokenResponse = await response.json();
-    if (response.status !== 200) {
-      throw response;
-    } else if (responseData.access_token) {
-      token = responseData.access_token;
 
-      return token;
-    } else if (responseData.statusText) {
-      return responseData.statusText;
+    if (response.status !== 200) {
+      return {
+        status: response.status,
+        statusText: `Request failed with status code ${response.status}`,
+      };
     }
 
-    return undefined;
-  } catch (error) {
-    const response = error as Response;
-    return {
-      status: response.status,
-      statusText: response.statusText,
-    };
+    if (!responseData.access_token) {
+      return {
+        status: 400, // Or a custom status code that fits this error situation
+        statusText: 'Failed to obtain access token',
+      };
+    }
+
+    return responseData.access_token;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        status: 500, // Assign a default status code
+        statusText: error.message,
+      };
+    }
+
+    throw new Error('Unknown error occurred.');
   }
 }
